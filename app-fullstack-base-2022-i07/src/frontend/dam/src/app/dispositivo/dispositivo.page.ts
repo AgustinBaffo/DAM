752f7c825c2fd9df0aeb1b40e82b65f7a0fac0a0
AgustinBaffo <abaffo@irobot.com>
2023-08-11 00:11:43 -0300

    add style to states

--- app-fullstack-base-2022-i07/src/frontend/dam/src/app/dispositivo/dispositivo.page.ts
+++ app-fullstack-base-2022-i07/src/frontend/dam/src/app/dispositivo/dispositivo.page.ts
@@ -30,5 +30,17 @@ export class DispositivoPage implements OnInit {
       })
     console.log(this.dispositivoMediciones);
   }
+
+  stateBackgroundColor(valor: number): { [key: string]: string } {
+    if (valor >= 0 && valor <= 10) {
+      return { backgroundColor: '#F08000', color: 'white' }; // Orange
+    } else if (valor > 10 && valor <= 30) {
+      return { backgroundColor: 'green', color: 'white' };
+    } else if (valor > 30 && valor <= 60) {
+      return { backgroundColor: '#FF4433', color: 'black' }; // Red
+    } else {
+      return { backgroundColor: 'gray', color: 'white' };
+    }
+  }
   
 }
