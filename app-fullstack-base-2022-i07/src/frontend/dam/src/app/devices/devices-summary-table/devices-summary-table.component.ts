517e54b362c76b14fd5ff356bad06c55fc56d518
AgustinBaffo <abaffo@irobot.com>
2023-08-19 20:07:32 -0300

    move request to summary table

--- app-fullstack-base-2022-i07/src/frontend/dam/src/app/devices/devices-summary-table/devices-summary-table.component.ts
+++ app-fullstack-base-2022-i07/src/frontend/dam/src/app/devices/devices-summary-table/devices-summary-table.component.ts
@@ -1,16 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DispositivoService } from 'src/app/services/dispositivo.service';
 
 @Component({
 	selector: 'app-devices-summary-table',
 	templateUrl: './devices-summary-table.component.html',
 	styleUrls: ['./devices-summary-table.component.scss'],
 })
+
 export class DevicesSummaryTableComponent implements OnInit {
 
-	@Input() devicesAndValues: any[] = [];
-	
-	constructor() { }
+	public devicesAndValues: any[] = [];
 
-	ngOnInit() { }
+	constructor(private dispositivoService: DispositivoService) { }
+
+	async ngOnInit() {
+		await this.dispositivoService.getListadoDispositivosConEstados()
+			.then((dispositivos) => {
+				console.log(dispositivos)
+				this.devicesAndValues = dispositivos;
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
 
 }
--- app-fullstack-base-2022-i07/src/frontend/dam/src/app/home/home.page.ts
+++ app-fullstack-base-2022-i07/src/frontend/dam/src/app/home/home.page.ts
@@ -1,35 +1,21 @@
 import { Component } from '@angular/core';
-import { DispositivoService } from '../services/dispositivo.service';
 import { MeasurementsSimulatorService } from '../services/measurements-simulator.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+	public simulation: boolean = false;
 
-  public devicesAndValues: any[] = [];  
-  public simulation: boolean = false;
-  
-  constructor(private dispositivoService: DispositivoService, private simService: MeasurementsSimulatorService) {}
+	constructor(private simService: MeasurementsSimulatorService) { }
 
-  async ngOnInit() {
-    await this.dispositivoService.getListadoDispositivosConEstados()
-      .then((dispositivos) => {
-        console.log(dispositivos)
-        this.devicesAndValues = dispositivos;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  toggleSimulation() {
-    if (this.simulation) {
-      this.simService.start();
-    } else {
-      this.simService.stop();
-    }
-  }
+	toggleSimulation() {
+		if (this.simulation) {
+			this.simService.start();
+		} else {
+			this.simService.stop();
+		}
+	}
 }
