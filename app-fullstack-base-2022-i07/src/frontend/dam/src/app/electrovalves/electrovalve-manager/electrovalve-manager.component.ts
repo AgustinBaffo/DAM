91f20bd1c84cc9904074f1092444ca277070529c
AgustinBaffo <abaffo@irobot.com>
2023-08-19 16:09:13 -0300

    fix electrovalves initial states

--- app-fullstack-base-2022-i07/src/frontend/dam/src/app/electrovalves/electrovalve-manager/electrovalve-manager.component.ts
+++ app-fullstack-base-2022-i07/src/frontend/dam/src/app/electrovalves/electrovalve-manager/electrovalve-manager.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
 import { IrrigationService } from 'src/app/services/irrigation.service';
 
 @Component({
@@ -6,25 +7,48 @@ import { IrrigationService } from 'src/app/services/irrigation.service';
 	templateUrl: './electrovalve-manager.component.html',
 	styleUrls: ['./electrovalve-manager.component.scss'],
 })
-export class ElectrovalveManagerComponent implements OnInit {
+export class ElectrovalveManagerComponent implements OnInit, OnChanges {
 
 	@Input() electrovalveId: number = 0;
 	public electrovalveOpen: number = 2; // Undefined
-	constructor(private _irrigationService: IrrigationService) { }
+
+	constructor(private _irrigationService: IrrigationService, private _actRouter: ActivatedRoute) { }
 
 	ngOnInit() { }
+
+	// @ todo: review it
+	// It can't be done in the ngOnInit because the inputs have no value at that point.
+	// Same thing with ngAfterViewInit.
+	// Since the page is not loading, ionViewWillEnter is not executed.
+	// I found this solution to make it work.
+	ngOnChanges(changes: SimpleChanges) {
+		if (changes['electrovalveId'] && !changes['electrovalveId'].firstChange) {
+			this.loadElectrovalveData();
+		}
+	}
+
+	async loadElectrovalveData() {
+		await this._irrigationService.getCurrentValueById(this.electrovalveId)
+			.then((ret) => {
+				this.electrovalveOpen = ret.currentValue;
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+
 
 	getElectrovalveImageSource(): string {
 		return './assets/electrovalve/electrovalve.jpg';
 	}
-	getElectrovalveDropImageSource(): string{
+	getElectrovalveDropImageSource(): string {
 		return './assets/electrovalve/drop.gif';
 	}
-	
+
 	toggleElectrovalve() {
 
 		// @todo: check valid state
-		const newStateOpened = this.electrovalveOpen===1 ? 0 : 1 ;
+		const newStateOpened = this.electrovalveOpen === 1 ? 0 : 1;
 
 		// @todo send command to real electrovalve (using async/await)
 		const ret = this.sendCommandToElectrovalve(newStateOpened);
