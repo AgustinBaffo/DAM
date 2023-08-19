90b0328b843133a02520ba07488e9b1ede15cd65
AgustinBaffo <abaffo@irobot.com>
2023-08-19 12:02:32 -0300

    move tables to another page

--- app-fullstack-base-2022-i07/src/frontend/dam/src/app/electrovalves/electrovalves-routing.module.ts
+++ app-fullstack-base-2022-i07/src/frontend/dam/src/app/electrovalves/electrovalves-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ElectrovalvesPage } from './electrovalves.page';
+import {IrrigationLogTableComponent} from './irrigation-log-table/irrigation-log-table.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ElectrovalvesPage
+  },
+  {
+    path: ':id/registers',
+    component: IrrigationLogTableComponent
   }
 ];
 
--- app-fullstack-base-2022-i07/src/frontend/dam/src/app/electrovalves/irrigation-log-table/irrigation-log-table.component.ts
+++ app-fullstack-base-2022-i07/src/frontend/dam/src/app/electrovalves/irrigation-log-table/irrigation-log-table.component.ts
@@ -1,19 +1,22 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+
 import { IrrigationService } from 'src/app/services/irrigation.service';
 
 @Component({
-  selector: 'app-irrigation-log-table',
-  templateUrl: './irrigation-log-table.component.html',
-  styleUrls: ['./irrigation-log-table.component.scss'],
+	selector: 'app-irrigation-log-table',
+	templateUrl: './irrigation-log-table.component.html',
+	styleUrls: ['./irrigation-log-table.component.scss'],
 })
-export class IrrigationLogTableComponent  implements OnInit {
-  
-  @Input() electrovalveId: number = 0;
+export class IrrigationLogTableComponent implements OnInit {
+
+	public electrovalveId: number = 0;
 	public logRegistersData: any[] = [];
 
-	constructor(private _irrigationService: IrrigationService) { }
+	constructor(private _irrigationService: IrrigationService, private _actRouter: ActivatedRoute) { }
 
 	async ngOnInit() {
+		this.electrovalveId = Number(this._actRouter.snapshot.paramMap.get('id'));
 		await this._irrigationService.getLogRegistersById(this.electrovalveId)
 			.then((registers) => {
 				for (let reg of registers) {
